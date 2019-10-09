010ad29dc9825675dd2ad68aa99b5e42260470b5
Brian Musisi <brianmusisi@yahoo.co.uk>
2019-10-09 22:14:07 +0300

    data file

--- assignment1/part2/viz/script.js
+++ assignment1/part2/viz/script.js
@@ -1,27 +1,27 @@
 
-const data_url = 'https://gist.githubusercontent.com/brianmusisi/76fa49b5f8267ec6754eed3854d7202f/raw/8c2bba9298fde15fb5d782eb4e99c2b07019e959/fitness_data.csv'
+var data_url = 'https://gist.githubusercontent.com/brianmusisi/76fa49b5f8267ec6754eed3854d7202f/raw/8c2bba9298fde15fb5d782eb4e99c2b07019e959/fitness_data.csv'
 
+var data_url = "https://gist.githubusercontent.com/brianmusisi/b81a71abf2a98f992efcb355143b035c/raw/709dff6fbe7a82e72d3b98e2b7143bd9bc38f150/daily_activities.json"
 d3.csv(data_url, function(error, data){
     if (error){
         console.log('An error occcurred');
     }
 
     else {
+    console.log(data)
     // bar width
     var barW = 25
 
     // margin and width objects
-    var margin = {top: 20, right: 20, bottom: 30, left: 50},
+    var margin = {top: 20, right: 50, bottom: 30, left: 80},
         width = 960 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
+        height = 300 - margin.top - margin.bottom;
 
     // x and y axis variables
     //var x = d3.scaleBand()
     //        .range([0, width])
     //        .padding(0.1);
     
-    var dataDates = data.map(d=> d.fitness_date)
-    //var dateRange = d3.extent()
     
     var x = d3.scaleTime()
               .range([0, width])
@@ -40,7 +40,7 @@ d3.csv(data_url, function(error, data){
 
     data.forEach( function(d){
         d.fitness_date = parseDate(d.date);
-        d.reduce_steps = d.steps /1000
+        d.reduce_steps = d.total_steps /1000
         
     })
 
@@ -59,6 +59,9 @@ d3.csv(data_url, function(error, data){
                 .attr("class", "tooltip")				
                 .style("opacity", 0);
 
+    //create colors
+    var blues = d3.scaleOrdinal(d3.schemeBlues[9]).domain([0, d3.max(data, function(d) { return d.steps; })]);
+    
     // create the bars
     var bars = svgBar.selectAll("rect")
                 .data(data, function(d, i){ return i; })
@@ -68,7 +71,8 @@ d3.csv(data_url, function(error, data){
                 .attr("y", function(d){ return y(d.reduce_steps)})
                 .attr("width", barW)
                 .attr("height", function(d) { return height - y(d.reduce_steps); })
-                .attr("fill", "steelblue");
+                .attr("fill", function(d) { return blues(d.steps)});
+                //.attr("fill", "steelblue");
     
     
     // create mouse over effect
@@ -80,7 +84,7 @@ d3.csv(data_url, function(error, data){
             .duration(200)		
             .style("opacity", .9)
 
-            div.html("Steps:  " + d.steps + "<br>" + "Distance:  " + Math.round(d.distance))
+            div.html("Steps:  " + d.steps + "<br>" + "Distance:  " + Math.round(d.total_distance))
             div
             .style("left", (d3.event.pageX) + "px")		
             .style("top", (y(d.reduce_steps) + 2 *(margin.top + margin.bottom)) + "px");
@@ -108,22 +112,21 @@ d3.csv(data_url, function(error, data){
     //y axis
     svgBar.append("g")
         .call(d3.axisLeft(y));
+
+    svgBar.append("text")      
+          .attr("y",30 - margin.left)
+          .attr("x",50 - (height / 2))
+          .attr("transform", "rotate(-90)")
+          .style("text-anchor", "end")
+          .style("font-size", "16px")
+          .text("Number of Steps ('000s)");
+
+
+    svgBar.append("text")      
+       .attr("x", (width / 2)+ 10)
+       .attr("y",  height + margin.bottom * 2)
+       .style("text-anchor", "middle")
+       .text("Date");
+
     }
 });
-
-const daily_activities_url = 'https://gist.githubusercontent.com/brianmusisi/b81a71abf2a98f992efcb355143b035c/raw/6fa39f3ad391236fd8908c6e8e26b05d301a3ff2/daily_activities.json'
-
-d3.json(daily_activities_url, function(error,data){
-
-    var barW = 25
-
-    // margin and width objects
-    var margin = {top: 20, right: 20, bottom: 30, left: 20},
-        width = 960 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
-
-    // x and y axis variables
-    var x = d3.scaleBand()
-            .range([0, width])
-            .padding(0.1);
-})
